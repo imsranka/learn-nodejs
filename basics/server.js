09c6e8c2d08154af7fa7707a2579000b3a9d5af9
imsranka <ranka.sakshi9@gmail.com>
2022-01-23 20:07:17 +0530

    sending data to client

--- basics/server.js
+++ basics/server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // request
+  let { url, method, headers } = req;
+  console.log(url, method);
+
+  // sending data
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    "<html><head><title>Hello</title></head><body><h1>First page</h1></body></html>"
+  );
+  res.end();
 });
 
 server.listen(3000);
